9b92f8239ce46933cf93ea6c149ca4e5f2b9a6ed
Tug0serya <dimasssss3@mail.ru>
2025-06-08 00:54:39 +1000

    ff

--- app.py
+++ app.py
@@ -1,18 +1,70 @@
-from flask import Flask, render_template
-from db import Add_to_db
+from flask import *
+from db import *
 
+date = '579'
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if True == 0:
+        nameOfForm = ''
+        #if request.form[nameOfForm] == "POST":
+        startTime = 'time_13_00'
+        endTime = 'time_14_30'
+        email = 'max lox'
+        flag = False
+
+        while True:
+            Update_element_db('TimeBook', startTime, 'day', email, date)
+            if startTime[8:] == '30':
+                startTime = startTime.replace(startTime[5:7], str(int(startTime[5:7]) + 1))
+                startTime = startTime.replace(startTime[8:], '00')
+            else:
+                startTime = startTime.replace(startTime[8:], '30')
+
+            if flag == True:
+                break
+
+            if startTime == endTime:
+                flag = True
+
+        return render_template("index.html")
+
+
+    elif True == False:
+        return redirect(url_for('profile'))
+
+    elif True == False:
+        return redirect(url_for('index'))
+
+    elif 'b' in request.form:
+        print(22)
+        return redirect(url_for('about'))
+
     return render_template("index.html")
+
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    if 1 == 1:
+        return render_template("about.html")
+    elif True == False:
+        return redirect(url_for('index'))
+
+
+
+@app.route("/profile")
+def profile():
+    if 1 == 1:
+        return render_template("profile_html")
+    elif True == False:
+        return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
-    Add_to_db('Users', 'dddd')
-    Add_to_db('Users', 'dfff')
-    app.run(debug=True)
+    #Add_to_db('Users', 'dfff')
+    #Delete_from_db('Users', 2)
+    #Update_element_db('Users', 'login', 3, 'rrrr')
+    app.run()
--- db.py
+++ db.py
@@ -6,13 +6,13 @@ cursor = connection.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-login TEXT NOT NULL);
+login TEXT UNIQUE NOT NULL);
 ''')
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Admins (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    login TEXT NOT NULL,
+    login TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 ''')
@@ -20,19 +20,87 @@ CREATE TABLE IF NOT EXISTS Admins (
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS BanUsers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    login TEXT NOT NULL
+    login TEXT UNIQUE NOT NULL
 );
 ''')
-connection.close()
-def Add_to_db(*args):
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS TimeBook (
+    day INTEGER PRIMARY KEY AUTOINCREMENT,
+    time_10_00 INTEGER,
+    time_10_30 INTEGER,
+    time_11_00 INTEGER,
+    time_11_30 INTEGER,
+    time_12_00 INTEGER,
+    time_12_30 INTEGER,
+    time_13_00 INTEGER,
+    time_13_30 INTEGER,
+    time_14_00 INTEGER,
+    time_14_30 INTEGER,
+    time_15_00 INTEGER,
+    time_15_30 INTEGER,
+    time_16_00 INTEGER,
+    time_16_30 INTEGER,
+    time_17_00 INTEGER,
+    time_17_30 INTEGER,
+    time_18_00 INTEGER,
+    time_18_30 INTEGER,
+    time_19_00 INTEGER,
+    time_19_30 INTEGER,
+    time_20_00 INTEGER,
+    time_20_30 INTEGER,
+    time_21_00 INTEGER,
+    time_21_30 INTEGER,
+    time_22_00 INTEGER
+);
+''')
+
+def Add_element_db(tableName, *columnNames, **values):
     connection = connect('db/dushess.db')
     cursor = connection.cursor()
-    cursor.execute("SELECT COUNT(*) FROM Users")
-    if args[0] == 'Users':
-        cursor.execute('INSERT INTO Users (login) VALUES (?)', (args[1],))
-    elif args[0] == 'Admins':
-        cursor.execute('INSERT INTO Admins (login, password) VALUES (?, ?)', (args[1], args[2]))
-    elif args[0] == 'BanUsers':
-        cursor.execute('INSERT INTO BanUsers (login) VALUES (?)', (args[1],))
+
+    countOfArg = ', '.join(['?'] * (len(values)))
+    columnsStr = ', '.join(columnNames)
+    print(list(values.values()))
+
+    cursor.execute(f'INSERT INTO {tableName} ({columnsStr}) VALUES ({countOfArg})', list(values.values()))
+    connection.commit()
+    connection.close()
+
+def Delete_element_db(tableName, deleteID):
+    connection = connect('db/dushess.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f'DELETE FROM {tableName} WHERE id = ?', (deleteID,))
+
+    connection.commit()
+    connection.close()
+
+def Update_element_db(tableName, columnSetType, indexColumn, replaceableElement, indexElement):
+    connection = connect('db/dushess.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f'UPDATE {tableName} SET {columnSetType} = ? WHERE {indexColumn} = ?', (replaceableElement, indexElement))
+
+    connection.commit()
+    connection.close()
+
+def Take_out_element_db(tableName, columnType, numberOfElement):
+    connection = connect('db/dushess.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f'SELECT {columnType} FROM {tableName} LIMIT {numberOfElement},1')
+    return cursor.fetchone()
+
+    connection.commit()
+    connection.close()
+
+def Take_out_column_db(tableName, columnType):
+    connection = connect('db/dushess.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f'SELECT {columnType} FROM {tableName}')
+    return cursor.fetchall()
+
     connection.commit()
     connection.close()
